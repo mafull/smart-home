43bec163b32880290c9d8cb6dd68cb6f293580a0
Max Fuller <max.fuller@cmrsurgical.com>
2019-04-09 21:49:02 +0100

    Exclude password hash when finding users

--- server/backend/src/controllers/users.js
+++ server/backend/src/controllers/users.js
@@ -3,7 +3,7 @@ import { Models } from "../database";
 
 const handleGETusers = async (req, res, next) => {
     try {
-        const users = await Models.User.findAll();
+        const users = await Models.User.findAll({ attributes: { exclude: ["passwordHash"] } });
         return res.status(200).send({ data: users });
     } catch (error) {
         return next(error);
